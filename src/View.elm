0f3d1cb3b59c8dc18c736e7fc5fd385c3eae5510
jadanis <joshallendanis@gmail.com>
2019-05-06 12:10:21 -0400

    Update View.elm

--- src/View.elm
+++ src/View.elm
@@ -13,3 +13,20 @@ import Svg
 import Svg.Attributes as SvgAttrs 
 
 
+renderBox : Cell Color -> Html Msg
+renderBox cell = 
+  Svg.rect
+    [ SvgAttrs.width (String.fromInt 30)
+    , SvgAttrs.height (String.fromInt 30)
+    , SvgAttrs.fill (Color.toString cell.val.toString)
+    , SvgAttrs.stroke (Color.toString cel.val.toString)
+    , SvgAttrs.strokeWidth "0.5"
+    , SvgAttrs.x (String.fromInt (Tuple.first cell.pos))
+    , SvgAttrs.y (String.fromInt (Tuple.second cell.pos))
+    ]
+    []
+
+
+renderGrid : Model -> Html Msg
+renderGrid {won, lost, playing, grid, seed} =
+  List.map renderBox grid
