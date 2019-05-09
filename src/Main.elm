1e0a148d5b87e1d119f516a5957d92dedc21aac0
jadanis <joshallendanis@gmail.com>
2019-05-09 10:03:09 -0400

    Update Main.elm

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,4 @@
 import Browser
-import Browser.Dom exposing (Viewport, getViewport)
-import Browser.Events exposing (onAnimationFrameDelta, onKeyDown, onKeyUp, onResize)
-
 import Messages exposing (..)
 import Model exposing (..)
 import View exposing (..)
