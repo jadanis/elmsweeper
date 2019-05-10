305c69693088b7cd84661a516642f87ce79d5cdd
jadanis <joshallendanis@gmail.com>
2019-05-10 10:29:16 -0400

    Update Grid.elm

--- src/Grid.elm
+++ src/Grid.elm
@@ -1,5 +1,7 @@
 module Grid exposing (..)
 
+import Json.Decode as Decode
+import Json.Encode as Encode
 
 type alias Cell a =
     { val : a
@@ -34,3 +36,39 @@ mapToList fun =
 empty : Grid a
 empty = 
     []
+
+
+
+decode : Decode.Decoder a -> Decode.Decoder (Grid a)
+decode cell =
+    Decode.list
+        (Decode.map6
+            Cell
+            (Decode.field "val" cell)
+            (Decode.field "pos" (Decode.map2 (\a b -> (a,b)) (Decode.index 0 Decode.int) (Decode.index 1 Decode.int)))
+            (Decode.field "mine" Decode.bool)
+            (Decode.field "flag" Decode.bool)
+            (Decode.field "neigh" Decode.int)
+            (Decode.field "rev" Decode.bool)
+        )
+
+
+encode : (a -> Encode.Value) -> Grid a -> Encode.Value
+encode cell grid =
+    let
+        encodeCell { val, pos, mine, flag, neigh, rev } =
+            Encode.object
+                [ ("val", cell val)
+                , ("pos"
+                  , Encode.list Encode.int
+                        [ Tuple.first pos
+                        , Tuple.second pos
+                        ]
+                  )
+                , ("mine",Encode.bool mine)
+                , ("flag", Encode.bool flag)
+                , ("neigh", Encode.int neigh)
+                , ("rev", Encode.bool rev)
+                ]
+    in
+        Encode.list encodeCell grid
