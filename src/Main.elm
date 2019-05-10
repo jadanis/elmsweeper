4132b985428ed9468f6659ea7be29ad337af6dee
jadanis <joshallendanis@gmail.com>
2019-05-10 10:30:08 -0400

    Update Main.elm

--- src/Main.elm
+++ src/Main.elm
@@ -1,17 +1,32 @@
+
 import Browser
 import Messages exposing (..)
 import Model exposing (..)
 import View exposing (..)
-
+import Time
+import Task
+import Random
 
 main =
-    Browser.sandbox
-        { init = Model.init
+    Browser.element
+        { init = init
         , update = Messages.update
         , view = View.view
+        , subscriptions = subscriptions
         }
 
 
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every 1000 Tick
 
-
-        
+init t = 
+    let
+        start = Time.millisToPosix t
+        seed = Random.initialSeed (modBy 100 t)
+        (grid,nseed) =
+            Model.newGame seed
+        i = Model.init
+        model = { i | start = start, curr = start, seed = nseed , grid = grid}
+    in
+        (model, Cmd.none)
